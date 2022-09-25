848df9d5f19380640d8828e03d0ab12753356a23
KALYANI <al.selvaganesan@gmail.com>
2022-09-25 14:44:11 +0530

    Update changes
    -add chatbot
    -add predict page

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from distutils.log import debug
 from flask import Flask, render_template, request
+import pickle
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def home():
     return render_template('index.html')
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        sg = float(request.form['sg'])
+
+        return render_template('predict.html',prediction=1)
 
 if __name__ == "__main__":
     app.run(debug = True)
