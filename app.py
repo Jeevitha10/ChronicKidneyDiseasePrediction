bc54472f3a12c979dcd63769a35f0f3e5c27ef1a
Jeevitha-10 <jeevitha.jayakumar@webinative.com>
2022-10-22 23:07:02 +0530

    Update form fields
    - Update the form fields in html
    - Update the values in app.py

--- app.py
+++ app.py
@@ -11,8 +11,19 @@ def home():
 @app.route("/predict", methods=['POST'])
 def predict():
     if request.method == 'POST':
-        sg = float(request.form['sg'])
-
+        bp = request.form['bp']
+        al = request.form['al']
+        su = request.form['su']
+        rbc = request.form['rbc']
+        pc = request.form['pc']
+        bgr = request.form['bgr']
+        sc = float(request.form['sc'])
+        pot = float(request.form['pot'])
+        hemo = float(request.form['hemo'])
+        htn = request.form['htn']
+        appet = request.form['appet']
+        pe = request.form['pe']
+        print(bp,al,su,rbc,pc,bgr,sc,pot,hemo,htn,appet,pe)
         return render_template('predict.html',prediction=1)
 
 if __name__ == "__main__":
